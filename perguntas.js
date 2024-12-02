3293cffdfb8b0a58b05612ce6a05be4464595e57
corvina777 <bosoriojapecanga@gmail.com>
2024-12-02 08:40:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior Estdo brasileiro em extensão territorial?',
+    'Amazonas'
+)
+
+criaCartao(
+    'Cultura',
+    'qual a principal bebida gaúcha?',
+    'O chimarrão'
+)
+
+criaCartao(
+    'Entretenimento',
+    'No filme Interestelar, qual foi o famoso "efeito borboleta?"',
+    'O momento em que o pai da Murphy sai ou fica da missão ao lado de astronautas'
+)
+
+criaCartao(
+    'Anime',
+    'Death Note" é o nome de um caderno com poderes Capazes de... ?',
+    'Matar quem ter seu nome escrito nele'
+)
+criaCartao(
+    'Mátematica',
+    'Quanto é 70% de 100',
+    '70'
+    
+)
+criaCartao(
+    'Geografia',
+    'Qual era o nome do primeiro continente',
+    'Pangeia'
+    
+)
